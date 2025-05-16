f1eaf06417e2590b3c48d7b85536d9fd3e99dcc3
Sahil <imsahil440407@gmail.com>
2025-05-16 12:29:36 +0530

    Slider added to homepage

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,8 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
         // Optional parameters
         direction: 'horizontal',
         loop: true,
+        slidesPerView: 3,
+        spaceBetween: 10,
 
         // Navigation arrows
         navigation: {
